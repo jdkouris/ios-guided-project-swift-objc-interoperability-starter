c02b10d1cd46a91d9d506db62e3a5fdbf197ce2d
John Kouris <jdkouris@gmail.com>
2020-02-11 19:46:39 -0800

    Added a starting implementation to the person controller

--- Star-Wars-Objc-Hybrid/Star-Wars-Hybrid/Model/PersonController.swift
+++ Star-Wars-Objc-Hybrid/Star-Wars-Hybrid/Model/PersonController.swift
@@ -7,3 +7,39 @@
 //
 
 import Foundation
+
+class PersonController: NSObject {
+    
+    private let baseURL = URL(string: "https://swapi.co/api/people/")!
+    
+    func searchForPeople(with searchTerm: String, completion: @escaping ([LSIPerson]?, Error?) -> Void) {
+        var components = URLComponents(url: baseURL, resolvingAgainstBaseURL: true)!
+        let searchItem = URLQueryItem(name: "search", value: searchTerm)
+        components.queryItems = [searchItem]
+        
+        let url = components.url!
+        
+        URLSession.shared.dataTask(with: url) { (data, _, error) in
+            if let error = error {
+                DispatchQueue.main.async {
+                    completion(nil, error)
+                }
+                return
+            }
+            
+            guard let data = data else {
+                DispatchQueue.main.async {
+                    completion(nil, NSError(domain: "PersonControllerErrorDomain", code: 0, userInfo: nil))
+                }
+                return
+            }
+            
+            do {
+                
+            } catch {
+                
+            }
+        }.resume()
+    }
+    
+}
