6d1e0719c4e535647337aec5f7d7a4a3d3738906
John Kouris <jdkouris@gmail.com>
2020-02-11 18:40:46 -0800

    Added a model controller and bridging header

---
+++ Contacts-Swift-Hybrid/Contacts-Hybrid/ContactsController.h
@@ -0,0 +1,17 @@
+//
+//  ContactsController.h
+//  Contacts-Hybrid
+//
+//  Created by John Kouris on 2/11/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ContactsController : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Contacts-Swift-Hybrid/Contacts-Hybrid/ContactsController.m
@@ -0,0 +1,13 @@
+//
+//  ContactsController.m
+//  Contacts-Hybrid
+//
+//  Created by John Kouris on 2/11/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import "ContactsController.h"
+
+@implementation ContactsController
+
+@end
